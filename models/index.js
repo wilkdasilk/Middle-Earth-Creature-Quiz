13f2e1b113178d982d06360295756d0c41a311d4
rccacho <rccacho@gmail.com>
2017-02-16 14:13:30 -0800

    created creature schema and connected all models files

--- models/index.js
+++ models/index.js
@@ -2,5 +2,7 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/Project-01");
 
 var User = require('./user');
+var Creature = require('./creature');
 
 module.exports.User = User;
+module.exports.Creature = Creature;
