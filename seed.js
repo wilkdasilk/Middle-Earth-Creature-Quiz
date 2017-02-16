01e533e80c887346af4015842e9df67cc8ae16fb
rccacho <rccacho@gmail.com>
2017-02-16 14:57:42 -0800

    added some sample user and creature data

--- seed.js
+++ seed.js
@@ -2,3 +2,30 @@
 // simply run: `node seed.js` from the root of this project folder.
 
 var db = require("./models");
+
+var sampleUsers = [];
+
+sampleUsers.push({
+	name: "Frodo",
+	city: "The Shire",
+	age: 50,
+	gender: "Male",
+	favoriteColor: "blue",
+	favoriteFood: "stew"
+})
+sampleUsers.push({
+	name: "Pippin",
+	city: "The Shire",
+	age: 28,
+	gender: "Male",
+	favoriteColor: "yellow",
+	favoriteFood: "bread"
+})
+
+var sampleCreatures = [];
+
+sampleCreatures.push({
+	creatureType: "Hobbit",
+	description: "Hobbits are small creatures that can be mistaken for young children. They love comfort and food and a good pipe. Although not accustomed to adventure, they can be both clever and surprisingly courageous, and also make great thieves as they are quiet and can sneak around unnoticed.",
+	madlib: "Enter madlib here!!"
+})
