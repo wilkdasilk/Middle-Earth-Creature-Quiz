022385a8cdbd9ce60c4ffb3d5990379f96872a1d
rccacho <rccacho@gmail.com>
2017-02-16 23:02:37 -0800

    set up template for create, destroy, and update functions

--- controllers/creaturesController.js
+++ controllers/creaturesController.js
@@ -1,5 +1,6 @@
 var db = require('../models');
 
+//GET ALL creatures and return as JSON
 function index(req,res){
   db.Creature.find({}, function(err, creatures){
     if (err){
@@ -11,7 +12,28 @@ function index(req,res){
   });
 }
 
+
+//POST 
+function create(req, res) {
+
+}
+
+
+//DELETE
+function destroy(req, res) {
+
+}
+
+
+//PUT
+function update(req, res) {
+
+}
+
+
 module.exports = {
   index: index,
-
+  create: create,
+  update: update,
+  destroy: destroy
 };
