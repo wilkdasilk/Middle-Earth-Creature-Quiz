37b5cc43fd2de3d8b7bb39d48550ca1db266ca1b
wilkdasilk <anwilk92@gmail.com>
2017-02-17 10:37:42 -0800

    required form fields

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -25,22 +25,22 @@ console.log("sanity check: JS connected!");
       <div class="userData">
         <form>
           <div class="row">
-            <div class="col-md-offset-3 col-md-3"><label>Name</label></div><div class="col-md-3"><input type="text" name="name"></div>
+            <div class="col-md-offset-3 col-md-3"><label>Name</label></div><div class="col-md-3"><input type="text" name="name" required></div>
           </div>
           <div class="row">
-            <div class="col-md-offset-3 col-md-3"><label>City</label></div><div class="col-md-3"><input type="text" name="city"></div>
+            <div class="col-md-offset-3 col-md-3"><label>City</label></div><div class="col-md-3"><input type="text" name="city" required></div>
           </div>
           <div class="row">
-            <div class="col-md-offset-3 col-md-3"><label>Age</label></div><div class="col-md-3"><input type="text" name="age"></div>
+            <div class="col-md-offset-3 col-md-3"><label>Age</label></div><div class="col-md-3"><input type="text" name="age" required></div>
           </div>
           <div class="row">
-            <div class="col-md-offset-3 col-md-3"><label>Gender</label></div><div class="col-md-3"><input type="radio" value="male" name="gender">Male <input type="radio" value="female" name="gender">Female</div>
+            <div class="col-md-offset-3 col-md-3"><label>Gender</label></div><div class="col-md-3"><input type="radio" value="male" name="gender" required>Male <input type="radio" value="female" name="gender">Female</div>
           </div>
           <div class="row">
-            <div class="col-md-offset-3 col-md-3"><label>Favorite Color</label></div><div class="col-md-3"><input type="text" name="favoriteColor"></div>
+            <div class="col-md-offset-3 col-md-3"><label>Favorite Color</label></div><div class="col-md-3"><input type="text" name="favoriteColor" required></div>
           </div>
           <div class="row">
-            <div class="col-md-offset-3 col-md-3"><label>Favorite Food</label></div><div class="col-md-3"><input type="text" name="favoriteFood"></div>
+            <div class="col-md-offset-3 col-md-3"><label>Favorite Food</label></div><div class="col-md-3"><input type="text" name="favoriteFood" required></div>
           </div>
           <input type="hidden" name="creature" value="Hobbit">
           <div class="row">
