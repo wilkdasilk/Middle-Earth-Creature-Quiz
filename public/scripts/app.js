c5e5d21237c4207c1bd77e7c49a0bed088631278
wilkdasilk <anwilk92@gmail.com>
2017-02-17 17:06:48 -0800

    modal sends successfully and closes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,6 +16,8 @@ console.log("sanity check: JS connected!");
     $mainDiv.empty();
   }
 
+  var formHTML;
+
   //Appends the result and form to the page
   function loadCreaturePage(creatureType){
 
@@ -30,6 +32,32 @@ console.log("sanity check: JS connected!");
       creatures.forEach(function(creature){
         if(creature.creatureType == creatureType){
           creatureType = creature;
+           formHTML = `
+          <form>
+            <div class="row">
+              <div class="col-md-offset-3 col-md-3"><label>Name</label></div><div class="col-md-3"><input type="text" name="name" required></div>
+            </div>
+            <div class="row">
+              <div class="col-md-offset-3 col-md-3"><label>City</label></div><div class="col-md-3"><input type="text" name="city" required></div>
+            </div>
+            <div class="row">
+              <div class="col-md-offset-3 col-md-3"><label>Age</label></div><div class="col-md-3"><input type="text" name="age" required></div>
+            </div>
+            <div class="row">
+              <div class="col-md-offset-3 col-md-3"><label>Gender</label></div><div class="col-md-3"><input type="radio" value="male" name="gender" required>Male <input type="radio" value="female" name="gender">Female</div>
+            </div>
+            <div class="row">
+              <div class="col-md-offset-3 col-md-3"><label>Favorite Color</label></div><div class="col-md-3"><input type="text" name="favoriteColor" required></div>
+            </div>
+            <div class="row">
+              <div class="col-md-offset-3 col-md-3"><label>Favorite Food</label></div><div class="col-md-3"><input type="text" name="favoriteFood" required></div>
+            </div>
+            <input type="hidden" name="creature" value="${creatureType.creatureType}">
+            <div class="row">
+              <div class="col-md-offset-3 col-md-3"><input id="formSubmit" type="submit"></div>
+            </div>
+          </form>
+          `;
           $mainDiv.append(`
             <div class="creature">
               <img src='${creatureType.imageUrl}'>
@@ -37,30 +65,7 @@ console.log("sanity check: JS connected!");
               <p>${creatureType.description}</p>
             </div>
             <div class="userData">
-              <form>
-                <div class="row">
-                  <div class="col-md-offset-3 col-md-3"><label>Name</label></div><div class="col-md-3"><input type="text" name="name" required></div>
-                </div>
-                <div class="row">
-                  <div class="col-md-offset-3 col-md-3"><label>City</label></div><div class="col-md-3"><input type="text" name="city" required></div>
-                </div>
-                <div class="row">
-                  <div class="col-md-offset-3 col-md-3"><label>Age</label></div><div class="col-md-3"><input type="text" name="age" required></div>
-                </div>
-                <div class="row">
-                  <div class="col-md-offset-3 col-md-3"><label>Gender</label></div><div class="col-md-3"><input type="radio" value="male" name="gender" required>Male <input type="radio" value="female" name="gender">Female</div>
-                </div>
-                <div class="row">
-                  <div class="col-md-offset-3 col-md-3"><label>Favorite Color</label></div><div class="col-md-3"><input type="text" name="favoriteColor" required></div>
-                </div>
-                <div class="row">
-                  <div class="col-md-offset-3 col-md-3"><label>Favorite Food</label></div><div class="col-md-3"><input type="text" name="favoriteFood" required></div>
-                </div>
-                <input type="hidden" name="creature" value="${creatureType.creatureType}">
-                <div class="row">
-                  <div class="col-md-offset-3 col-md-3"><input type="submit"></div>
-                </div>
-              </form>
+              ${formHTML}
             </div>
             <div class="allUsers">
             </div>
@@ -137,14 +142,32 @@ console.log("sanity check: JS connected!");
         });
       });
 
+      //Opens modal when edit button is clicked
       $(".editBtn").on('click', function(event) {
-        console.log("clicked to edit");
         var currentUserId = $(this).parent().data('user-id');
         $('#userModal').data('user-id', currentUserId);
         $('#userModal').modal();
+        $('#modal-form-content').html(formHTML);
+        $('#formSubmit').remove();
+        $('#saveChangesBtn').click(function(event){
+          event.preventDefault();
+          $.ajax({
+            method: "PUT",
+            url: '/api/users/' + currentUserId,
+            data: $('form').serialize(),
+            success: updateSuccess,
+            error: onError
+          });
+        })
       });
     }
   }
+
+function updateSuccess(updatedUser){
+  $('#userModal').modal('hide');
+  console.log("updated Successfully");
+  console.log(updatedUser);
+}
 
 
 //Removes its profile from the page
